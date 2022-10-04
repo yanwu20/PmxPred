50034f6d28f47cdcf3cdef034bdf8f856283eb38
xiaoyu <136710101562163.com>
2022-10-04 12:41:17 +1100

    model update

--- PmxPred.py
+++ PmxPred.py
@@ -1,10 +1,14 @@
 import argparse
 import warnings
 import pandas as pd
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem,Descriptors
 from rdkit import Chem
 import pickle
 import numpy as np
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from rdkit.ML.Descriptors import MoleculeDescriptors
+from imblearn.pipeline import Pipeline as imbpipeline
 
 
 def FingerPrint(mols):
@@ -18,54 +22,47 @@ def FingerPrint(mols):
     return fp_MorganFingerprint
 
 
-def Smile2FP(FileName):
-    df_input = pd.read_csv(FileName)
-    mols = [Chem.MolFromSmiles(m) for i, m in df_input.iloc[:, 0].items()]
+def Smile2FP(df):
+    mols = [Chem.MolFromSmiles(m.values[0]) for i, m in df.iterrows()]
     fp_MorganFingerprint = FingerPrint(mols)
-    fp_MorganFingerprint["label"] = df_input["Label"]
+    # fp_MorganFingerprint["label"] =df["Label"]
     return fp_MorganFingerprint
 
 
-def Smile2CDK(FileName):
-    r = robjects.r
-    r(
-        '''
-        library("rcdk")
-        f <- function(r){
-         input_data<-read.csv(r)
-         Smiles<-input_data[1]
-         dc <- get.desc.categories()
-         desc_list<-c("org.openscience.cdk.qsar.descriptors.molecular.BCUTDescriptor")
-         for (num in 2:5){
-             dn<- get.desc.names(dc[num])
-             for (i in 1:length(dn)){
-                     desc_list[length(desc_list)+1]<-dn[i]
-             }
-         }
-         Smiles_v<-unlist(Smiles)
-         m <- parse.smiles(Smiles_v)
-         descs <- eval.desc(m, desc_list)
-         return(descs)
-         }
-
-        '''
-    )
-    df_CDK = r['f'](FileName)
-
-    return df_CDK
+def calc_chiral(mol):
+    chiral_list = []
+    for i, atom in enumerate(mol.GetAtoms()):
+        chiral_list.append(str(atom.GetChiralTag()))
+    return Counter(chiral_list)
 
 
-def prediction(df_CDK, df_MFP, isolate):
-    model = pickle.load(open("./model/model", "rb"))
-    CDK_model = model["CDK"]
-    MFP_model = model["MFP"]
-    model_RDK = CDK_model[isolate]
-    model_FP = MFP_model[isolate]
+def Smile2RDK(df):
+    chiral_name = ["CHI_OTHER", "CHI_TETRAHEDRAL_CCW", "CHI_TETRAHEDRAL_CW", "CHI_UNSPECIFIED"]
+    des_name = [x[0] for x in Descriptors._descList]
+    calculator = MoleculeDescriptors.MolecularDescriptorCalculator(des_name)
+    out_list = []
+    mols = [Chem.MolFromSmiles(m.values[0]) for i,m in df.iterrows()]
+    for mol in mols:
+        if mol:
+            descriptors = list(calculator.CalcDescriptors(mol))
+            chiral_dict = calc_chiral(mol)
+            for k in chiral_name:
+                descriptors.append(chiral_dict[k])
+            out_list.append(descriptors)
+    des_name.extend(chiral_name)
+    df_out = pd.DataFrame(out_list, columns=des_name)
+    # df_out["label"] = df["Label"]
+    return df_out
+
+
+def prediction(X_RDK, X_MFP, isolate):
+    model = pickle.load(open("./model", "rb"))
+    RDK_model = model[isolate]["RDK"]
+    MFP_model = model[isolate]["FP"]
 
     output_dic = dict()
-    output_dic["RDK"] = model_RDK.predict_proba(df_CDK)[:, 1]
-    output_dic["FP"] = model_FP.predict_proba(df_MFP)[:, 1]
-
+    output_dic["RDK"] =RDK_model.predict_proba(X_RDK)[:,1]
+    output_dic["FP"] = MFP_model.predict_proba(X_MFP)[:,1]
     df_level1 = pd.DataFrame(output_dic)
     df_level1["score"] = 0.5 * df_level1["RDK"] + 0.5 * df_level1["FP"]
     df_level1["Label"] = np.rint(df_level1["score"])
@@ -76,7 +73,7 @@ if __name__ == "__main__":
     # PmxPred
 
     warnings.filterwarnings("ignore")
-    parser = argparse.ArgumentParser(description='generate feature')
+    parser = argparse.ArgumentParser(description='Prediction')
     parser.add_argument("--type", required=True,
                         choices=["benchmark", "predict"],
                         help="the encoding type")
@@ -89,9 +86,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.type == "benchmark":
-        df_input = pd.read_csv("./polymyxin data/%s_Binary.csv" %args.isolates)
-        print(prediction())
+        df_input = pd.read_csv("./polymyxin data/%s_Binary.csv" % args.isolate)
+        X, X_test, y, y_test = train_test_split(df_input.iloc[:, :-1], df_input.iloc[:, -1], test_size=0.2,
+                                                random_state=0, stratify=df_input.iloc[:, -1])
+        df_MFP = Smile2FP(X_test)
+        df_RDK = Smile2RDK(X_test)
+        df_result = prediction(df_RDK, df_MFP, args.isolate)
+        # df_result.to_csv(args.output_name)
+        print(df_result)
     else:
-        df_MFP = Smile2FP(args.SMILES_file)
-        df_CDK = Smile2CDK(args.SMILES_file)
-        print(prediction(df_CDK=df_CDK, df_MFP=df_MFP, isolate=args.isolate))
+        df_input = pd.read_csv(args.SMILES_file)
+        df_MFP = Smile2FP(df_input)
+        df_RDK = Smile2RDK(df_input)
+        df_result = prediction(df_RDK, df_MFP, args.isolate)
+        df_result.to_csv(args.output_name)
+        print(df_result)
