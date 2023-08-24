9c8c4c521b1a379e54b14976036e6136f920819e
yanwu20 <49023946+yanwu20@users.noreply.github.com>
2023-08-24 20:06:31 +1000

    Update PmxPred.py

--- PmxPred.py
+++ PmxPred.py
@@ -1,72 +1,45 @@
 import argparse
 import warnings
 import pandas as pd
-from rdkit.Chem import AllChem,Descriptors
-from rdkit import Chem
 import pickle
 import numpy as np
-from collections import Counter
-from sklearn.model_selection import train_test_split
-from rdkit.ML.Descriptors import MoleculeDescriptors
-from imblearn.pipeline import Pipeline as imbpipeline
+import torch.nn.functional as F
+from utils import *
+from torch_geometric.loader import DataLoader
 
 
-def FingerPrint(mols):
-    fingerprints = []
-    safe = []
-    for mol_idx, mol in enumerate(mols):
-        fingerprint = [x for x in AllChem.GetMorganFingerprintAsBitVect(mol, 3, 2048)]
-        fingerprints.append(fingerprint)
-        safe.append(mol_idx)
-    fp_MorganFingerprint = pd.DataFrame(fingerprints).fillna(0)
-    return fp_MorganFingerprint
 
+def file2test(df, feature_name):
+    all_residue = []
+    index_list = []
+    for i, smile in df.iterrows():
+        data = Smile2Graph_global_ali_new(smile[0], 0, feature_name)
+        if data:
+            all_residue.append(data)
+            index_list.append(i)
+        else:
+            print("number %s is not polymyxin analogue" %i)
+    return all_residue, index_list
 
-def Smile2FP(df):
-    mols = [Chem.MolFromSmiles(m.values[0]) for i, m in df.iterrows()]
-    fp_MorganFingerprint = FingerPrint(mols)
-    # fp_MorganFingerprint["label"] =df["Label"]
-    return fp_MorganFingerprint
+def file2data_ali_new(df, feature_name):
+    all_residue = []
+    for i, (smile, y) in df.iterrows():
+        data = Smile2Graph_global_ali_new(smile, y, feature_name)
+        if data:
+            all_residue.append(data)
+    return all_residue
 
-
-def calc_chiral(mol):
-    chiral_list = []
-    for i, atom in enumerate(mol.GetAtoms()):
-        chiral_list.append(str(atom.GetChiralTag()))
-    return Counter(chiral_list)
-
-
-def Smile2RDK(df):
-    chiral_name = ["CHI_OTHER", "CHI_TETRAHEDRAL_CCW", "CHI_TETRAHEDRAL_CW", "CHI_UNSPECIFIED"]
-    des_name = [x[0] for x in Descriptors._descList]
-    calculator = MoleculeDescriptors.MolecularDescriptorCalculator(des_name)
+def test(test_loader, model):
     out_list = []
-    mols = [Chem.MolFromSmiles(m.values[0]) for i,m in df.iterrows()]
-    for mol in mols:
-        if mol:
-            descriptors = list(calculator.CalcDescriptors(mol))
-            chiral_dict = calc_chiral(mol)
-            for k in chiral_name:
-                descriptors.append(chiral_dict[k])
-            out_list.append(descriptors)
-    des_name.extend(chiral_name)
-    df_out = pd.DataFrame(out_list, columns=des_name)
-    # df_out["label"] = df["Label"]
-    return df_out
-
-
-def prediction(X_RDK, X_MFP, isolate):
-    model = pickle.load(open("./model", "rb"))
-    RDK_model = model[isolate]["RDK"]
-    MFP_model = model[isolate]["FP"]
-
-    output_dic = dict()
-    output_dic["RDK"] =RDK_model.predict_proba(X_RDK)[:,1]
-    output_dic["FP"] = MFP_model.predict_proba(X_MFP)[:,1]
-    df_level1 = pd.DataFrame(output_dic)
-    df_level1["score"] = 0.5 * df_level1["RDK"] + 0.5 * df_level1["FP"]
-    df_level1["Label"] = np.rint(df_level1["score"])
-    return df_level1
+    model.eval()
+    for data in test_loader:  # Iterate in batches over the training/test dataset.
+        with torch.no_grad():
+            data = data.cuda()
+            x = F.normalize(data.x, p=1.0, dim=0)
+            out = model(x, data.edge_index, data.batch)
+            out_list.extend(out.tolist())
+    node_score = [item for sublist in out_list for item in sublist]
+    return node_score
 
 
 if __name__ == "__main__":
@@ -79,25 +52,57 @@ if __name__ == "__main__":
                         help="the encoding type")
     parser.add_argument("--strain", required=True,
                         choices=["A. baumannii ATCC 19606", "E. coli ATCC 25922", "K. pneumoniae ATCC 13883",
-                                 "P. aeruginosa ATCC 27853", "all"],
+                                 "P. aeruginosa ATCC 27853", "Gram negative"],
                         help="the name of strain")
+    parser.add_argument("--combine",
+                        choices=["LR", "Min"],default = "LR",
+                        help="the combine type")
     parser.add_argument('--SMILES_file', type=str, help='input SMILES file')
     parser.add_argument('--output_name', type=str, help='output file name')
     args = parser.parse_args()
 
+    batch_size = 4
+
     if args.type == "benchmark":
-        df_input = pd.read_csv("./polymyxin data/%s_Binary.csv" % args.strain)
-        X, X_test, y, y_test = train_test_split(df_input.iloc[:, :-1], df_input.iloc[:, -1], test_size=0.2,
-                                                random_state=0, stratify=df_input.iloc[:, -1])
-        df_MFP = Smile2FP(X_test)
-        df_RDK = Smile2RDK(X_test)
-        df_result = prediction(df_RDK, df_MFP, args.strain)
-        df_result.to_csv(args.output_name)
-        print(df_result)
+        df_input = pd.read_csv("./Data/%s_Binary_test.csv" % args.strain, index_col=0)
+        test_set = file2data_ali_new(df_input, "Des")
+        test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+        test_label = [d.y.item() for d in test_set]
+        index_list = df_input.index
+
     else:
         df_input = pd.read_csv(args.SMILES_file)
-        df_MFP = Smile2FP(df_input)
-        df_RDK = Smile2RDK(df_input)
-        df_result = prediction(df_RDK, df_MFP, args.strain)
-        df_result.to_csv(args.output_name)
-        print(df_result)
+        # print(df_input.shape)
+        test_set, index_list = file2test(df_input, "Des")
+        test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+    # node-level model
+    model = torch.load("./models/GCN_ali_%s_" % args.strain).cuda()
+    node_score = test(test_loader, model)
+
+    # global feature model
+    with open("./cv_results/catBoost_%s_model" % args.strain, "rb") as f:
+        global_cat_model = pickle.load(f)
+
+    global_x_test = [d.global_feature.tolist() for d in test_set]
+    global_score = global_cat_model.predict_proba(global_x_test)[:, 1]
+
+    df_result = pd.DataFrame([global_score, node_score], columns=index_list, index=["Global socre","node score"]).T
+
+    combine_type = "LR"
+    if combine_type == "Min":
+        score_list = [min(s1, s2) for s1, s2 in zip(global_score, node_score)]
+        pred_list = [1 if s > 0.5 else 0 for s in score_list]
+    elif combine_type == "Mean":
+        score_list = [(s1+s2)/2 for s1, s2 in zip(global_score, node_score)]
+        pred_list = [1 if s > 0.5 else 0 for s in score_list]
+
+    elif combine_type == "LR":
+        with open("./models/LR_%s" % args.strain, "rb") as f:
+            clf = pickle.load(f)
+        score_list = clf.predict_proba(df_result)[:, 1]
+        pred_list = clf.predict(df_result)
+
+    df_result["Final"] = score_list
+    # df_result.to_csv(args.output_name)
+    print(df_result)
